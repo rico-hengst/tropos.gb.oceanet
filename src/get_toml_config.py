c1e58f279702c4b598319b951c0067f4f4ad9133
Rico Hengst <rico.hengst@tropos.de>
2021-07-05 11:52:07 +0200

    move location2TSI

--- src/get_toml_config.py
+++ src/get_toml_config.py
@@ -26,25 +26,23 @@ import pandas as pd
 import pytz
 import datetime
 import logging
+import pprint
 
 
-""" Create logger, name important """
-module_logger = logging.getLogger('oceanet-processing.get_config')
-module_logger.setLevel(logging.INFO)
-logging.basicConfig(level=logging.INFO)
+
 
 
 # set pathfile of config files (toml))
-cfg_instruments_file = "conf/instruments.toml"
-cfg_missions_file = "conf/missions.toml"
+cfg_instruments_file = os.path.dirname(os.path.realpath(__file__))  + "/../conf/instruments.toml"
+cfg_missions_file = os.path.dirname(os.path.realpath(__file__))  + "/../conf/missions.toml"
 
 
 # check if config files exists
 if not os.path.isfile( cfg_instruments_file ):
-    module_logger.warn( 'File not exists '+ cfg_instruments_file )
+    logger.warn( 'File not exists '+ cfg_instruments_file )
     quit()
 if not os.path.isfile( cfg_missions_file ):
-    module_logger.warn( 'File not exists '+ cfg_missions_file )
+    logger.warn( 'File not exists '+ cfg_missions_file )
     quit()
 
 
@@ -58,7 +56,7 @@ cfg_missions = toml.load(cfg_missions_file)
 for mission in cfg_missions:
     #print(mission)
     if not ( cfg_missions.get(mission,{}).get("datetime_start") and cfg_missions.get(mission,{}).get("datetime_stopp") ):
-        module_logger.info("start stopp date of mission not exists??")
+        logger.info("start stopp date of mission not exists??")
         quit()
     """pandas time counter vector instead of loop"""   
     dates=pd.date_range(
@@ -72,18 +70,19 @@ for mission in cfg_missions:
                 
     # add hidden key _dates with date vectors to each mission
     
-    cfg_missions[mission]['_dates'] = dates.to_pydatetime()
+    #cfg_missions[mission]['_dates'] = dates.to_pydatetime()
+    cfg_missions[mission]['_'] = { 'dates' :dates.to_pydatetime() }
 
 
 
 
-def get_instruments( mission_of_interest ):
-    module_logger.info("Start to collect related instruments to mission of interest: " + mission_of_interest )
-    module_logger.info("Show metadata starting from selected mission -> get instruments")
+def get_instruments( mission_of_interest, logger ):
+    logger.info("Start to collect related instruments to mission of interest: " + mission_of_interest )
+    logger.info("Show metadata starting from selected mission -> get instruments")
     
     # check if mission_of_interest exists
     if not cfg_missions.get(mission_of_interest, {}):
-        module_logger.warn("Error, mission not exists: " + mission_of_interest)
+        logger.warn("Error, mission not exists: " + mission_of_interest)
         quit()
 
     
@@ -103,14 +102,14 @@ def get_instruments( mission_of_interest ):
 
 
 
-def get_missions( instrument_of_interest ):
-    module_logger.info("Start to collect related missions to instrument of interest: " + instrument_of_interest )
-    module_logger.info("Show metadata starting from selected instrument -> get missions")
+def get_missions( instrument_of_interest, logger ):
+    logger.info("Start to collect related missions to instrument of interest: " + instrument_of_interest )
+    logger.info("Show metadata starting from selected instrument -> get missions")
 
 
     # check if instrument_of_interest exists
     if not cfg_instruments.get(instrument_of_interest, {}):
-        module_logger.warn("Error, instrument not exists: " + instrument_of_interest)
+        logger.warn("Error, instrument not exists: " + instrument_of_interest)
         quit()
 
     # add instrument to nested dict
@@ -134,7 +133,7 @@ def add_instrument_pathfilenames(args, cfg):
     # get _dates from mission
     if (args.selected == "mission"):
         for mission in cfg["mission"]:
-            dates = cfg["mission"][mission]["_dates"]
+            dates = cfg["mission"][mission]["_"]["dates"]
 
     
         # get pathfile from instrument, add to instrument
@@ -166,17 +165,17 @@ def add_instrument_pathfilenames(args, cfg):
 
 # ###################################################################################
 #
-def loop(args):
+def loop(args, logger):
     nested_dict = None
     
     if (args.selected == "instrument"):
-        module_logger.info("Instrument was choosed")
-        nested_dict = get_missions( args.name )
+        logger.info("Instrument was choosed")
+        nested_dict = get_missions( args.name, logger )
     elif (args.selected == "mission"):
-        module_logger.info("Mission was choosed")
-        nested_dict = get_instruments( args.name )
+        logger.info("Mission was choosed")
+        nested_dict = get_instruments( args.name, logger )
     else:
-        module_logger.warn("error")
+        logger.warn("error")
         quit()
         
     cfg = toml.loads( toml.dumps(nested_dict)  )
@@ -185,10 +184,15 @@ def loop(args):
 
 
     #print (cfg)
-    module_logger.info("RESULTED CONFIG:\n")
+    #logger.info("RESULTED CONFIG:\n")
     for key in cfg:
-        module_logger.info(key + ":")
-        module_logger.info( cfg.get(key,[]) )
+        logger.info(key + ":")
+       # logger.info( cfg.get(key,[]) )
+        
+        
+        pp = pprint.PrettyPrinter(width=71, compact=True, depth=3,  indent=3)
+
+        pp.pprint(cfg.get(key,[]) )
         
     return cfg
 
@@ -198,6 +202,17 @@ def loop(args):
 # getting args, setting logger, load configs
 def adjust(argv):
     print("___")
+    
+    # get name of directory where main script is located
+    current_dirname = os.path.dirname(os.path.realpath(__file__))
+    
+    # get the name of the directory from where the script was executed
+    exec_dirname = os.getcwd()
+    
+    # define log_path_file + create dir
+    #log_path_file = current_dirname + "/log/uv_processing.log"
+    log_path_file = current_dirname + "/../log/oceanet.log"
+    
     """for calling the function from the terminal"""
     parser = argparse.ArgumentParser(description='Oceanet, read config.') 
     
@@ -207,20 +222,50 @@ def adjust(argv):
     parser.add_argument('-n', required=True, type=str, dest='name', # la variable se guarda en args.id como string
                     help='Insert the name of the instrument or mission')
 
-    #parser.add_argument('--loglevel', default='INFO', dest='loglevel',
-                    #help="define loglevel of screen INFO (default) | WARNING | ERROR ")
+    parser.add_argument('--loglevel', default='INFO', dest='loglevel',
+                    help="define loglevel of screen INFO (default) | WARNING | ERROR ")
     #parser.add_argument('--logfile', default=log_path_file, dest='logfile',
                     #help="define logfile (default: directory where you execute the script) ")
     args = parser.parse_args()
     
     
+    """ Create logger, name important """
+ 
+    # create logger with
+    logger = logging.getLogger('oceanet')
+    logger.setLevel(logging.DEBUG)
     
-    module_logger.info("Start to read config files")
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler( log_path_file )
+    fh.setLevel(logging.DEBUG)
+    
+    # create/check level
+    screen_level = logging.getLevelName(args.loglevel)
+    
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    #ch.setLevel(logging.WARNING)
+    ch.setLevel(screen_level)
+    
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter(fmt='%(asctime)s | %(name)-14s | %(levelname)-8s | %(message)s | %(module)s (%(lineno)d)', datefmt='%Y-%m-%d %H:%M:%S',)
+    
+    # add formatter to the handlers
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
     
     
-    cfg = loop( args )
     
-    module_logger.info("Finish to read config files")
+    logger.info("Start to read config files")
+    
+    
+    cfg = loop( args, logger )
+    
+    logger.info("Finish to read config files")
     
     return cfg
     
